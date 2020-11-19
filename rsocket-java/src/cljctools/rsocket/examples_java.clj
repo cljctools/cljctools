e86a67d8177cd0ba71d49ce8fc7010fe0b010d57
seeris <sergei.udris@gmail.com>
2020-11-19 11:29:25 +0300

    rsocket-java: rename examples.clj -> examples-java.clj to avoid collision with runtimeless examples

--- rsocket-java/src/cljctools/rsocket/examples_java.clj
+++ rsocket-java/src/cljctools/rsocket/examples_java.clj
@@ -1,4 +1,4 @@
-(ns cljctools.rsocket.examples
+(ns cljctools.rsocket.examples-java
   (:require
    [clojure.core.async :as a :refer [chan go go-loop <! >!  take! put! offer! poll! alt! alts! close!
                                      pub sub unsub mult tap untap mix admix unmix pipe
