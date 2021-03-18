c25a6d5ce14722beef5dc1a419ab10bc70c0b465
Sergei Udris <sergei.udris@gmail.com>
2021-03-18 09:30:53 +0300

    java net socket  hello-worl-echo test

--- src/socket-test/src/cljctools/socket/java_net_test.clj
+++ src/socket-test/src/cljctools/socket/java_net_test.clj
@@ -39,6 +39,7 @@
       (is (= -4 (+ -2 -2)))
       (is (= -1 (+ 3 -4))))))
 
+
 (deftest hello-world-echo
   (testing "io/read io/write"
     (let [server-recv| (chan 10)
@@ -52,6 +53,33 @@
                           (.bind (java.net.InetSocketAddress. host 0)))
           port (.getLocalPort socket-server)
 
+          socket-send
+          (fn socket-send
+            [id socket send|]
+            (go
+              (with-open [writer (io/writer socket)]
+                (loop []
+                  (when-let [data (<! send|)]
+                    (println ::will-send data)
+                    (.write writer (pr-str data))
+                    (.newLine writer)
+                    (.flush writer)
+                    (recur))))
+              (println ::closing-socket-send id)))
+
+          socket-recv
+          (fn socket-recv
+            [id socket recv|]
+            (go
+              (with-open [reader (io/reader socket)]
+                (try
+                  (loop []
+                    (let [line (.readLine reader)]
+                      (put! recv| (read-string line))
+                      (recur)))
+                  (catch IOException ex (println ::readline (ex-message ex)))))
+              (println ::closing-socket-recv id)))
+
           release (fn []
                     (close! server-recv|)
                     (close! server-send|)
@@ -61,64 +89,34 @@
       (println ::port port)
       (go
         (loop []
-          (when-let [line (<! server-recv|)]
-            (let [data (read-string line)]
-              (println ::server-recv data)
-              (put! server-send| data))
+          (when-let [data (<! server-recv|)]
+            (println ::server-recv data)
+            (put! server-send| data)
             (recur))))
       (go
-        (loop []
-          (let [socket (.accept socket-server)
-                writer (io/writer socket)
-                reader (io/reader socket)]
-            (go
-              (let [server-send|tap (tap server-send|mult (chan 10))]
-                (loop []
-                  (when-let [data (<! server-send|tap)]
-                    (println ::will-send data)
-                    (.write writer (pr-str data))
-                    (.newLine writer)
-                    (.flush writer)
-                    (recur))))
+        (try
+          (loop [socketsA (atom #{})]
+            (if-not (.isClosed socket-server)
               (do
-                (println ::closing-client-socket)
-                (.close socket)
-                (.close writer)
-                (.close reader)))
-            (go
-              (loop []
-                (let [line (.readLine reader)]
-                  (put! server-recv| line)
-                  (recur)))))
-          (recur)))
-      (go
-        (let [socket (doto (java.net.Socket.)
-                       (.connect (java.net.InetSocketAddress. host port)))
-              writer (io/writer socket)
-              reader (io/reader socket)]
-          (println (type writer))
-          (println (type reader))
-          (go
-            (loop []
-              (when-let [data (<! socket-send|)]
-                (.write writer (pr-str (merge data)))
-                (.newLine writer)
-                (.flush writer)
-                (recur)))
-            (do
-              (println ::closing-socket)
-              (.close socket)
-              (.close writer)
-              (.close reader)))
-          (go
-            (loop []
-              (let [line (.readLine reader)]
-                (put! socket-recv| (read-string line))
-                (recur))))))
-      (let [data {::foo 42}]
+                (let [socket (.accept socket-server)]
+                  (socket-send ::socket-client socket (tap server-send|mult (chan 10)))
+                  (socket-recv ::socket-client socket server-recv|)
+                  (recur (swap! socketsA conj socket))))
+              (do
+                (doseq [socket @socketsA]
+                  (.close socket)))))
+          (catch IOException ex (println ::socket-accept (ex-message ex)))))
+      (let [data {::foo 42}
+            socket| (go
+                      (with-open [socket (doto (java.net.Socket.)
+                                           (.connect (java.net.InetSocketAddress. host port)))]
+                        (<! (a/merge [(socket-send ::socket socket socket-send|)
+                                      (socket-recv ::socket socket socket-recv|)]))
+                        (println ::will-close-socket)))]
         (put! socket-send| data)
         (let [echo (<!! socket-recv|)]
           (println ::echo echo)
           (is (=  data echo)))
         (println ::release)
-        (release)))))
+        (release)
+        (<!! socket|)))))
