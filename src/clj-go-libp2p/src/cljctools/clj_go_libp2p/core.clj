fc0a21b4a6175894d6b00a8fc396cbc9097d7352
Sergei Udris <sergei.udris@gmail.com>
2021-04-09 07:32:08 +0300

    add deps.edn and /src

---
+++ src/clj-go-libp2p/src/cljctools/clj_go_libp2p/core.clj
@@ -0,0 +1,13 @@
+(ns cljctools.clj-go-libp2p.core
+  (:require
+   [clojure.core.async :as a :refer [chan go go-loop <! >! <!! >!!  take! put! offer! poll! alt! alts! close!
+                                     pub sub unsub mult tap untap mix admix unmix pipe
+                                     timeout to-chan  sliding-buffer dropping-buffer
+                                     pipeline pipeline-async]]
+   [clojure.string]
+   [clojure.spec.alpha :as s]))
+
+
+(defn create
+  []
+  (println ::baz))
