0cd979e7cc66ce7662a6d556d86916fa4c8a1447
Sergei Udris <sergei.udris@gmail.com>
2021-05-18 11:11:28 +0300

    add :reload to cljs (require ..)

--- src/datagram-socket-nodejs/src/cljctools/datagram_socket/core.cljs
+++ src/datagram-socket-nodejs/src/cljctools/datagram_socket/core.cljs
@@ -72,9 +72,9 @@
   (require '[clojure.core.async :as a :refer [chan go go-loop <! >!  take! put! offer! poll! alt! alts! close! onto-chan!
                                               pub sub unsub mult tap untap mix admix unmix pipe
                                               timeout to-chan  sliding-buffer dropping-buffer
-                                              pipeline pipeline-async]])
+                                              pipeline pipeline-async]] :reload)
   
-  (require '[cljctools.datagram-socket.core :as datagram-socket.core])
+  (require '[cljctools.datagram-socket.core :as datagram-socket.core] :reload)
   
   
   ;
--- src/socket-nodejs/src/cljctools/socket/core.cljs
+++ src/socket-nodejs/src/cljctools/socket/core.cljs
@@ -82,9 +82,9 @@
   (require '[clojure.core.async :as a :refer [chan go go-loop <! >!  take! put! offer! poll! alt! alts! close! onto-chan!
                                               pub sub unsub mult tap untap mix admix unmix pipe
                                               timeout to-chan  sliding-buffer dropping-buffer
-                                              pipeline pipeline-async]])
+                                              pipeline pipeline-async]] :reload)
   
-  (require '[cljctools.socket.core :as socket.core])
+  (require '[cljctools.socket.core :as socket.core] :reload)
   
   
   ;
